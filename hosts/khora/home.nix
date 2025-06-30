af9c053e24da12b55b6ec60ff71e28ea6ac471f9
btoschek <btoschek@protonmail.com>
2025-06-30 22:03:34 +0200

    home(lazygit): Add lazygit

--- hosts/khora/home.nix
+++ hosts/khora/home.nix
@@ -108,6 +108,8 @@
     pinentry.package = pkgs.pinentry-curses;
   };
 
+  programs.lazygit.enable = true;
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
