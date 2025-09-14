067167fb53c220aa8b8cf548bcbd83c7288d1391
btoschek <btoschek@protonmail.com>
2025-09-14 15:22:36 +0200

    general: Remove unnecessary comments

--- hosts/khora/configuration.nix
+++ hosts/khora/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { inputs, pkgs, ... }:
 
 {
@@ -35,21 +31,23 @@
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Automatic updating
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.dates = "weekly";
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
+  };
 
   # Automatic cleanup
-  nix.gc.automatic = true;
-  nix.gc.dates = "daily";
-  nix.gc.options = "--delete-older-than 10d";
-  nix.settings.auto-optimise-store = true;
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 10d";
+    };
+    settings.auto-optimise-store = true;
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -57,12 +55,6 @@
     # font = "Consolas";
     keyMap = "de";
     useXkbConfig = false; # use xkb.options in tty.
-  };
-
-  # NOTE: This is needed, regardless of using X11 or not
-  services.xserver = {
-    enable = true;
-    displayManager.lightdm.enable = false;
   };
 
   programs.hyprland = {
@@ -97,23 +89,11 @@
     sane.enable = true;
   };
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
+  # Enable sound
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.btoschek = {
@@ -171,34 +151,21 @@
     sops
     makemkv
 
+    simple-scan
+
     gcc
+    libreoffice-qt
+    pgadmin4-desktopmode
+
+    orca-slicer
+
+    nfs-utils
   ];
 
   programs.zsh.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.pcscd.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   #enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 8000 ]; # Occasional python http.server
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
--- hosts/khora/home.nix
+++ hosts/khora/home.nix
@@ -26,13 +26,7 @@
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
     pkgs.nerd-fonts.hack
   ];
 
@@ -115,22 +109,7 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/btoschek/etc/profile.d/hm-session-vars.sh
-  #
+  # Environment variables available to shells managed by home-manager
   home.sessionVariables = {
     EDITOR = "nvim";
   };
