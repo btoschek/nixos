9193ad4a0545e710a360ea3145886d66fd96b31f
btoschek <btoschek@protonmail.com>
2025-09-14 13:54:28 +0200

    khora(hardware): Add external drive for video storage

--- hosts/khora/hardware-configuration.nix
+++ hosts/khora/hardware-configuration.nix
@@ -38,6 +38,11 @@
       fsType = "ext4";
     };
 
+  fileSystems."/home/btoschek/Videos" =
+    { device = "/dev/disk/by-uuid/71d0d13a-d4a0-4c81-8ec9-d6d5947daea1";
+      fsType = "ext4";
+    };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
