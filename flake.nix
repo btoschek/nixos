4f5b470677773e0de79e594fcdd58c5226ecfcd2
btoschek <btoschek@protonmail.com>
2026-01-19 18:42:20 +0100

    general: Reduce amount of flake dependencies

--- flake.nix
+++ flake.nix
@@ -34,7 +34,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    impermanence.url = "github:nix-community/impermanence";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
   };
 
   outputs = { self, nixpkgs, sops-nix, impermanence, ... }@inputs:
